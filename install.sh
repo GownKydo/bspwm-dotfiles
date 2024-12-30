66ca801cc6a957afd57a52daf58c220953b2ff59
Gerardo H. Gomez <corniergown@gerardohgomez0112@gmail.com>
2024-12-30 18:18:29 -0500

    add function for detect the system and install dependencies

--- install.sh
+++ install.sh
@@ -1,35 +1,83 @@
 #!/bin/bash
 
-greenColour="\e[0;32m\033[1m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
-endColour="\033[0m\e[0m"
+function get_package_manager() {
+    if command -v pacman &>/dev/null; then
+        echo "pacman"
+    elif command -v apt &>/dev/null; then
+        echo "apt"
+    elif command -v dnf &>/dev/null; then
+        echo "dnf"
+    elif command -v zypper &>/dev/null; then
+        echo "zypper"
+    else
+        echo "unknown"
+    fi
+}
 
 function check_dependencies() {
     echo -e "${yellowColour}[*] Cheking dependencies in the system ...${endColour}\n"
 
     local packages=("bspwm" "sxhkd" "polybar" "rofi" "i3lock" "amixer" "zsh" "brightnessctl" \
-                    "feh" "picom" "neofetch" "cowsay" "ranger" "alacritty" "lolcat")
+                    "feh" "picom" "neofetch" "cowsay" "ranger" "alacritty" "lolcat" "htop")
+
+    local successfully=0
+    local failed=0
+    local missing_dependencies=()
 
     for package in "${packages[@]}"; do
         if command -v "$package" &>/dev/null; then
-            echo -e "${greenColour}[INSTALLED] - ${endColour} ${package}"
+            echo -e "\t${greenColour}[INSTALLED]${endColour} - ${package}"
+            ((successfully++))
         else
-            echo -e "${redColour}[NOT FOUND] - ${endColour} $package. Please install the package."
+            echo -e "\t${redColour}[NOT FOUND]${endColour} - $package. Please install the package."
+            ((failed++))
+            missing_dependencies="$package"
+
         fi
         sleep 0.5
     done
 
-    echo -e "\n ${yellowColour}The verification of the packages has been successfully completed!${endColour} \n"
+    echo -e "\n${yellowColour} - $successfully packages found, $failed packages not found ${endColour}"
+    
+    if [[ $failed -gt 0 ]]; then
+        echo -e "${yellowColour}[*] Do you want to install the missing dependencies[y/n]? ${endColour}"
+        read -e option
+
+        if [[ $option == "YES" || $option == "yes" || $option == "Y" || $option == "y" ]]; then
+            local pkg_manager=$(get_package_manager)
+
+            case "$pkg_manager" in
+                "pacman")
+                    for dependencie in "${missing_dependencies[@]}"; do
+                        pacman -S --noconfirm "$dependencie"
+                    done
+                    ;;
+                "apt")
+                    for dependencie in "${missing_dependencies[@]}"; do
+                        sudo apt update && sudo apt install -y "$dependencie"
+                    done
+                    ;;
+                "dnf")
+                    for dependencie in "${missing_dependencies[@]}"; do
+                        sudo dnf install -y "$dependencie"
+                    done
+                    ;;
+                "zypper")
+                    for dependencie in "${missing_dependencies[@]}"; do
+                        sudo zypper install -y "$dependencie"
+                    done
+                    ;;
+                *)
+                    echo -e "${redColour}No compatible package manager found. Unable to install dependencies.${endColour}"
+                    ;;
+            esac
+        fi
+    fi
 }
 
 function check_and_handle_folders() {
     local base_dir=$(find $HOME/ -depth -name dotfiles -type d -print -quit)
-    local folders=("bspwm" "nvim" "picom" "polybar" "rofi" "sxhkd")
+    local folders=("bspwm" "picom" "polybar" "rofi" "sxhkd")
     
     mkdir -p /home/$USER/.config/backup_config
 
@@ -43,6 +91,21 @@ function check_and_handle_folders() {
     done
 }
 
-clear
-check_dependencies
-check_and_handle_folders
+function main() {
+    # Clear Screen
+    clear
+    
+    # Call Functions 
+    check_dependencies
+    check_and_handle_folders
+}
+
+# Variables for colors
+greenColour="\e[0;32m\033[1m"
+redColour="\e[0;31m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+endColour="\033[0m\e[0m"
+
+
+# Call the main function
+main
