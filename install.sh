28b4706093206af3c0338e28f9e6b4eeda5552d2
Gerardo H. Gomez <corniergown@gerardohgomez0112@gmail.com>
2025-01-05 04:16:06 -0500

    add changes

--- install.sh
+++ install.sh
@@ -18,7 +18,8 @@ function check_dependencies() {
     echo -e "${yellowColour}[*] Cheking dependencies in the system ...${endColour}\n"
 
     local packages=("bspwm" "sxhkd" "polybar" "rofi" "i3lock" "amixer" "zsh" "brightnessctl" \
-                    "feh" "picom" "neofetch" "cowsay" "ranger" "alacritty" "lolcat" "htop")
+                    "feh" "picom" "neofetch" "cowsay" "ranger" "alacritty" "lolcat" "htop" \
+                    "flameshot" "lsd" "bat" "zsh")
 
     local successfully=0
     local failed=0
@@ -29,10 +30,9 @@ function check_dependencies() {
             echo -e "\t${greenColour}[INSTALLED]${endColour} - ${package}"
             ((successfully++))
         else
-            echo -e "\t${redColour}[NOT FOUND]${endColour} - $package. Please install the package."
+            echo -e "\t${redColour}[NOT FOUND]${endColour} - $package."
             ((failed++))
             missing_dependencies="$package"
-
         fi
         sleep 0.5
     done
@@ -72,29 +72,36 @@ function check_dependencies() {
                     ;;
             esac
         fi
+        echo -e "${yellowColour}\¿n[*] Dependencies correctly installed! ${endColour}"
     fi
 }
 
 function check_and_handle_folders() {
     local base_dir=$(find $HOME/ -depth -name dotfiles -type d -print -quit)
-    local folders=("bspwm" "picom" "polybar" "rofi" "sxhkd")
+    local folders=("bspwm" "picom" "polybar" "neofetch" "rofi" "sxhkd" "neofetch")
     
+    echo -e "${yellowColour}[*] Creating backup config in ${greenColour}\"$HOME/.config/backup_config\" ${endColour}"
     mkdir -p /home/$USER/.config/backup_config
 
     for folder in "${folders[@]}"; do        
         if [ -d $HOME/.config/$folder ]; then
+            echo -e "${yellowColour}Moving ${folder} directory to ${greenColour}\"$Home/config/backup_config\" ${endColour}"
             mv $HOME/.config/$folder $HOME/.config/backup_config
         fi
-
+        sleep 0.5
+        echo -e "$\n${yellowcolour}Creating direcotry \"${folder}\" ${endColour}"
         mkdir $HOME/.config/$folder
+
+        echo -e "\n${yellowColour}Coping config files . . .${endColour}\n"
         cp -r "$base_dir/config/$folder" "$HOME/.config/$folder"
+        sleep 0.5
     done
 }
 
 function main() {
     # Clear Screen
     clear
-    
+
     # Call Functions 
     check_dependencies
     check_and_handle_folders
@@ -105,7 +112,6 @@ greenColour="\e[0;32m\033[1m"
 redColour="\e[0;31m\033[1m"
 yellowColour="\e[0;33m\033[1m"
 endColour="\033[0m\e[0m"
-
 
 # Call the main function
 main
